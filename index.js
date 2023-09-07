91822e1f6a06820802bcfb3460e48a9167c5e996
Izadora <izadorasantosdesousa@gmail.com>
2023-09-07 10:30:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const routes = require("./routes/userRouter");
 const conn = require("./db/conn");
 app.use(express.json());
